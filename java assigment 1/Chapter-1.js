d7decbc9c7b420c54cfc67851793f3e52997dc99
Ahsan-Abdul-Samad <145339197+Ahsan-Abdul-Samad@users.noreply.github.com>
2025-02-22 23:33:45 +0500

    Add files via upload

---
+++ java assigment 1/Chapter-1.js
@@ -0,0 +1,20 @@
+// question num 1
+
+alert("Wellcome to my website");
+
+// question num 2
+
+alert("Error! Please Enter a valid password.");
+
+// question num 3
+
+alert("Welcome to JS Land... \n Happy Coding!");
+
+// question number 4
+
+alert("Welcome to JS Land");
+alert("Happy Coding! \n Pervent this page from creating additional dialogs");
+
+// question num 5
+
+alert("Hello... I can run JS through my google chrome browser's console")
